b28dc7e5ed5084e5c5d4b14dc791539841a89354
Aman_Kumar_shaw_5 <ashaw8176@gmail.com>
2026-07-30 19:54:42 +0530

    methods for service layer added in interfaces

--- src/main/java/app/aman/BookHaven/service/InventoryService.java
+++ src/main/java/app/aman/BookHaven/service/InventoryService.java
@@ -1,4 +1,9 @@
 package app.aman.BookHaven.service;
 
+import app.aman.BookHaven.entity.Room;
+
 public interface InventoryService {
+    void initializeRoomForAYear(Room room);
+
+    void deleteFutureInventories(Room room);
 }
