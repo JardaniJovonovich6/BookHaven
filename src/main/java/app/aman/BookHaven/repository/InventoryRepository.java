3cac20cbf99e6c2a270b515508c8a6ecab37f0c3
Aman_Kumar_shaw_5 <ashaw8176@gmail.com>
2026-08-05 15:25:55 +0530

    DeletefutureInventories for InventoryServiceImpl done

--- src/main/java/app/aman/BookHaven/repository/InventoryRepository.java
+++ src/main/java/app/aman/BookHaven/repository/InventoryRepository.java
@@ -1,10 +1,14 @@
 package app.aman.BookHaven.repository;
 
 import app.aman.BookHaven.entity.Inventory;
+import app.aman.BookHaven.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 //added all three repositories
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory , Long> {
+    void deleteByDateAfterAndRoom(LocalDate today, Room room);
 }
